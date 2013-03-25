dc90e7d142470bdd20a3f84c1e6d7b5e0c8bfce5
michael.praschl@gmail.com <michael.praschl@gmail.com@2611e95c-66b2-ff4a-e29e-84b7ea985aed>
2013-03-25 21:26:52 +0000

    Added a default constructor to AliasesAttribute for cls compliancy

--- MiP.ShellArgs/AutoWireAttributes/AliasesAttribute.cs
+++ MiP.ShellArgs/AutoWireAttributes/AliasesAttribute.cs
@@ -6,10 +6,19 @@ namespace MiP.ShellArgs.AutoWireAttributes
     /// <summary>
     /// Used to specify aliases for an option.
     /// </summary>
+    [SuppressMessage("Microsoft.Design", "CA1019:DefineAccessorsForAttributeArguments")]
     [AttributeUsage(AttributeTargets.Property)]
     public sealed class AliasesAttribute : Attribute
     {
         private string[] _aliases;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AliasesAttribute"/> class.
+        /// </summary>
+        public AliasesAttribute()
+        {
+            _aliases = new string[0];
+        }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="AliasesAttribute"/> class.
@@ -24,6 +33,6 @@ namespace MiP.ShellArgs.AutoWireAttributes
         /// Gets or sets the aliases for an option.
         /// </summary>
         [SuppressMessage("Microsoft.Performance", "CA1819:PropertiesShouldNotReturnArrays")]
-        public string[] Aliases { get { return _aliases; } private set { _aliases = value ?? new string[0]; } }
+        public string[] Aliases { get { return _aliases; } set { _aliases = value ?? new string[0]; } }
     }
 }
